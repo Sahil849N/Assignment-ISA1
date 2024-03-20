e8bbd212b8642637972800e01e22fe4cb72c17cb
Sahil Nadaf <130697216+Sahil849N@users.noreply.github.com>
2024-03-20 07:14:37 +0530

    Update 10_Array_Operations.c

--- 10_Array_Operations.c
+++ 10_Array_Operations.c
@@ -2,52 +2,59 @@
 and average of an array.*/
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+
+int main() {
     printf("Enter the size of the array\n");
-    int n, ch, sum = 0, maxi = 0;
+    int n, ch, sum = 0, maxi = 0, mini = 0;
     float avg = 0;
     scanf("%d", &n);
     int array[n];
     printf("Enter Array Elements : ");
     for (int i = 0; i < n; i++)
         scanf("%d", &array[i]);
-    maxi = array[0];
-    while (1)
+    maxi = mini = array[0];
+    while (1) 
     {
-        printf("Enter 1 : Sum\nEnter 2 : Average\nEnter 3 : Largest\nEnter 4 : Smallest\n");
+        printf("Enter 1 : Sum\nEnter 2 : Average\nEnter 3 : Largest\nEnter 4 : Smallest\nEnter 0 : Exit\n");
         scanf("%d", &ch);
 
-        switch (ch)
-        {
-        case 1:
-            for (int i = 0; i < n; i++)
-                sum += array[i];
-            printf("Sum : %d\n", sum);
-            break;
+        switch (ch) {
+            case 1:
+                for (int i = 0; i < n; i++)
+                    sum += array[i];
+                printf("Sum : %d\n", sum);
+                break;
 
-        case 2:
-            for (int i = 0; i < n; i++)
-                sum += array[i];
-            avg = sum / n;
-            printf("Average : %f\n", avg);
-            break;
+            case 2:
+                for (int i = 0; i < n; i++)
+                    sum += array[i];
+                avg = ((float)sum )/((float) n);
+                printf("Average : %.2f\n", avg);
+                break;
 
-        case 3:
-            for (int i = 1; i < n; i++)
-            {
-                if (maxi < array[i])
-                    maxi = array[i];
-            }
-            printf("Largest : %d\n", maxi);
+            case 3:
+                for (int i = 1; i < n; i++) {
+                    if (maxi < array[i])
+                        maxi = array[i];
+                }
+                printf("Largest : %d\n", maxi);
+                break;
 
-        case 4:
-            for (int i = 1; i < n; i++)
-            {
-                if (maxi > array[i])
-                    maxi = array[i];
-            }
-            printf("Smallest : %d\n", maxi);
+            case 4:
+                mini = array[0];
+                for (int i = 1; i < n; i++) {
+                    if (mini > array[i])
+                        mini = array[i];
+                }
+                printf("Smallest : %d\n", mini);
+                break;
+
+            case 0:
+                exit(0);
+                break;
+
+            default:
+                printf("Invalid choice\n");
         }
     }
     return 0;
